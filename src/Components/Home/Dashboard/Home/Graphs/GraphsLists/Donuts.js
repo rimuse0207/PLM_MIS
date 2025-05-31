b30ce07031f56c5566a5764a9da982b31f03bcec
Yoo Sung Jae <rimuse0207@naver.com>
2025-05-31 12:42:12 +0900

    Pie animate 수정

--- src/Components/Home/Dashboard/Home/Graphs/GraphsLists/Donuts.js
+++ src/Components/Home/Dashboard/Home/Graphs/GraphsLists/Donuts.js
@@ -36,68 +36,27 @@ const Donuts = () => {
     return (
         <div style={{ width: '100%', height: '100%', textAlign: 'center' }}>
             <ResponsivePie
-                /**
-                 * chart에 사용될 데이터
-                 */
                 data={data}
-                /**
-                 * chart margin
-                 */
                 margin={{ top: 40, right: 0, bottom: 80, left: 0 }}
-                /**
-                 * chart 중간 빈공간 반지름
-                 */
                 innerRadius={0}
-                /**
-                 * pad 간격
-                 */
                 padAngle={1}
-                /**
-                 * pad radius 설정 (pad별 간격이 있을 시 보임)
-                 */
                 cornerRadius={0}
-                /**
-                 * chart 색상
-                 */
                 colors={['skyblue', 'orange', 'gray', 'gold', 'green', 'purple']} // 커스터하여 사용할 때
                 // colors={{ scheme: 'nivo' }} // nivo에서 제공해주는 색상 조합 사용할 때
-                /**
-                 * pad border 두께 설정
-                 */
                 borderWidth={0}
-                /**
-                 * link label skip할 기준 각도
-                 */
                 arcLinkLabelsSkipAngle={0}
-                /**
-                 * link label 색상
-                 */
                 arcLinkLabelsTextColor="#000000"
-                /**
-                 * link label 연결되는 선 두께
-                 */
                 arcLinkLabelsThickness={2}
-                /**
-                 * link label 연결되는 선 색상
-                 */
                 arcLinkLabelsColor={{ from: 'color' }} // pad 색상에 따라감
-                /**
-                 * label (pad에 표현되는 글씨) skip할 기준 각도
-                 */
                 arcLabelsSkipAngle={10}
+                animate={false}
                 theme={{
-                    /**
-                     * label style (pad에 표현되는 글씨)
-                     */
                     labels: {
                         text: {
                             fontSize: 14,
                             fill: '#000000',
                         },
                     },
-                    /**
-                     * legend style (default로 하단에 있는 색상별 key 표시)
-                     */
                     legends: {
                         text: {
                             fontSize: 12,
@@ -105,14 +64,6 @@ const Donuts = () => {
                         },
                     },
                 }}
-                /**
-                 * pad 클릭 이벤트
-                 */
-                // onClick={handle.padClick}
-                /**
-                 * legend 설정 (default로 하단에 있는 색상별 key 표시)
-                 */
-
                 legends={[
                     {
                         anchor: 'bottom', // 위치
@@ -128,16 +79,6 @@ const Donuts = () => {
                         symbolSize: 18, // symbol (색상 표기) 크기
                         symbolShape: 'circle', // symbol (색상 표기) 모양
                         justify: false,
-                        effects: [
-                            {
-                                // 추가 효과 설정 (hover하면 textColor를 olive로 변경)
-                                on: 'hover',
-                                style: {
-                                    itemTextColor: 'olive',
-                                },
-                            },
-                        ],
-                        // onClick: handle.legendClick, // legend 클릭 이벤트
                     },
                 ]}
                 layers={[
