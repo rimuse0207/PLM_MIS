3b979d7b10980de63b084b44ea4b150864e72885
Anne <rimuse0207@naver.com>
2025-12-03 14:46:39 +0900

    EIS 외주가공비 추가

--- src/Components/Home/Dashboard/Home/DepartTop/DepartLists/DepartLists.js
+++ src/Components/Home/Dashboard/Home/DepartTop/DepartLists/DepartLists.js
@@ -1,113 +1,158 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { FaWonSign } from 'react-icons/fa6';
-import * as XLSX from 'xlsx/xlsx.mjs';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { FaWonSign } from "react-icons/fa6";
+import * as XLSX from "xlsx/xlsx.mjs";
 
 const DepartListsMainDivBox = styled.div`
-    border: 1px solid lightgray;
-    border-left: 1px solid rgb(0, 202, 255);
-    width: 18%;
-    height: 170px;
-    background-color: #fff;
-    box-shadow: -8px 8px 3px -5px lightgray;
-    border-radius: 5px;
-    margin-top: 30px;
-    margin-bottom: 10px;
-    padding-left: 10px;
-    padding-right: 10px;
-    min-width: 260px;
-    position: relative;
-    &:hover {
-        cursor: pointer;
-        background-color: #efefef;
+  border: 1px solid lightgray;
+  border-left: 1px solid rgb(0, 202, 255);
+  width: 18%;
+  height: 170px;
+  background-color: #fff;
+  box-shadow: -8px 8px 3px -5px lightgray;
+  border-radius: 5px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  min-width: 260px;
+  position: relative;
+  &:hover {
+    cursor: pointer;
+    background-color: #efefef;
+  }
+  .Price_Container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h2 {
+      width: 60%;
+      text-align: center;
     }
-    .Price_Container {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        h2 {
-            width: 60%;
-            text-align: center;
-        }
-    }
-    .Money_Containers {
-        font-weight: bolder;
-        font-size: 1.1em;
-        text-align: end;
-    }
-    .Apper_Container {
-        position: absolute;
-        right: 10px;
-        top: 5px;
-        font-weight: bolder;
-    }
+  }
+  .Money_Containers {
+    font-weight: bolder;
+    font-size: 1.1em;
+    text-align: end;
+  }
+  .Apper_Container {
+    position: absolute;
+    right: 10px;
+    top: 5px;
+    font-weight: bolder;
+  }
 `;
 
 const DepartLists = ({ list }) => {
-    const Navigation = useNavigate();
+  const Navigation = useNavigate();
 
-    const HandleClick_GoTo_DetailPage = Select_Data => {
-        Navigation(`/Sub/Detail/${Select_Data.Department_code}`);
-    };
+  const HandleClick_GoTo_DetailPage = (Select_Data) => {
+    Navigation(`/Sub/Detail/${Select_Data.Department_code}`);
+  };
 
-    const HandleClicks = () => {
-        exportToExcel();
-    };
+  const HandleClicks = () => {
+    exportToExcel();
+  };
 
-    const exportToExcel = () => {
-        const workbook = XLSX.utils.book_new();
+  const exportToExcel = () => {
+    const workbook = XLSX.utils.book_new();
 
-        const filteredParts = list.Part_Price_Calcu.filter(part => part.ERP_PART.startsWith('R'));
-
-        if (filteredParts.length > 0) {
-            const worksheet = XLSX.utils.json_to_sheet(filteredParts);
-            XLSX.utils.book_append_sheet(workbook, worksheet, `${list.Models}`);
-        }
-
-        XLSX.writeFile(workbook, `${list.Models}.xlsx`);
-    };
-
-    return (
-        <DepartListsMainDivBox onDoubleClick={() => HandleClicks()}>
-            <div style={{ marginTop: '30px' }}>
-                <h2 style={{ display: 'inline' }}>{list.Segment}</h2>
-                <span>({list.WO_TYPE === 'B' ? 'Board' : '장비'})</span>
-            </div>
-            <div className="Apper_Container">
-                모델명: {list.Models} {list.WO_TYPE === 'B' ? `(${list.QTY}매)` : `#${list.CHNG_CONT.split('#')[1]}`}
-            </div>
-            <div className="Price_Container">
-                <h2 style={{ fontSize: '6vmin' }}>
-                    {list.MC_Price
-                        ? ((list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
-                              ? (list.MC_Price / list.EXPC_SEL_PRICE) * 100
-                              : (list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100
-                          ).toFixed(1)
-                        : 0}
-                    %
-                </h2>
-                <div style={{ width: '40%', borderLeft: '1px solid lightgray' }}>
-                    <div className="Money_Containers">
-                        <div style={{ fontSize: '0.8em', textAlign: 'start', marginLeft: '20px' }}>MC</div>
-                        <div>
-                            &#8361;{' '}
-                            {Number(
-                                (list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
-                                    ? list.MC_Price / 1000000
-                                    : ((list.MC_Price / 1000000) * list.QTY).toFixed(1)
-                            ).toLocaleString('ko-KR')}{' '}
-                            M
-                        </div>
-                    </div>
-                    <div className="Money_Containers" style={{ marginTop: '20px' }}>
-                        <div style={{ fontSize: '0.8em', textAlign: 'start', marginLeft: '20px' }}>판가</div>
-                        <div>&#8361; {(list.EXPC_SEL_PRICE / 1000000).toLocaleString('ko-KR')} M</div>
-                    </div>
-                </div>
-            </div>
-        </DepartListsMainDivBox>
+    const filteredParts = list.Part_Price_Calcu.filter((part) =>
+      part.ERP_PART.startsWith("R")
     );
+
+    if (filteredParts.length > 0) {
+      const worksheet = XLSX.utils.json_to_sheet(filteredParts);
+      XLSX.utils.book_append_sheet(workbook, worksheet, `ERP PART 단가`);
+    }
+
+    const filteredParts2 = list.outSoucingPrice;
+
+    if (filteredParts2.length > 0) {
+      const worksheet2 = XLSX.utils.json_to_sheet(filteredParts2);
+      XLSX.utils.book_append_sheet(
+        workbook,
+        worksheet2,
+        `ERP 외주 가공비 단가`
+      );
+    }
+
+    XLSX.writeFile(workbook, `${list.Models}.xlsx`);
+  };
+
+  return (
+    <DepartListsMainDivBox onDoubleClick={() => HandleClicks()}>
+      <div style={{ marginTop: "30px" }}>
+        <h2 style={{ display: "inline" }}>{list.Segment}</h2>
+        <span>({list.WO_TYPE === "B" ? "Board" : "장비"})</span>
+      </div>
+      <div className="Apper_Container">
+        모델명: {list.Models}{" "}
+        {list.WO_TYPE === "B"
+          ? `(${list.QTY}매)`
+          : `#${list.CHNG_CONT.split("#")[1]}`}
+      </div>
+      <div className="Price_Container">
+        <h2 style={{ fontSize: "6vmin" }}>
+          {list.MC_Price
+            ? ((list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
+                ? ((list.MC_Price + list.outSoucingPriceSum) /
+                    list.EXPC_SEL_PRICE) *
+                  100
+                : ((list.MC_Price * list.QTY + list.outSoucingPriceSum) /
+                    list.EXPC_SEL_PRICE) *
+                  100
+              ).toFixed(1)
+            : 0}
+          %
+        </h2>
+        <div style={{ width: "40%", borderLeft: "1px solid lightgray" }}>
+          <div className="Money_Containers">
+            <div
+              style={{
+                fontSize: "0.8em",
+                textAlign: "start",
+                marginLeft: "20px",
+              }}
+            >
+              MC
+            </div>
+            <div>
+              &#8361;{" "}
+              {Number(
+                (list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
+                  ? (
+                      (list.MC_Price + list.outSoucingPriceSum) /
+                      1000000
+                    ).toFixed(1)
+                  : (
+                      (list.MC_Price * list.QTY + list.outSoucingPriceSum) /
+                      1000000
+                    ).toFixed(1)
+              ).toLocaleString("ko-KR")}{" "}
+              M
+            </div>
+          </div>
+          <div className="Money_Containers" style={{ marginTop: "20px" }}>
+            <div
+              style={{
+                fontSize: "0.8em",
+                textAlign: "start",
+                marginLeft: "20px",
+              }}
+            >
+              판가
+            </div>
+            <div>
+              &#8361; {(list.EXPC_SEL_PRICE / 1000000).toLocaleString("ko-KR")}{" "}
+              M
+            </div>
+          </div>
+        </div>
+      </div>
+    </DepartListsMainDivBox>
+  );
 };
 
 export default DepartLists;
--- src/Components/Home/Dashboard/Home/Graphs/GraphsMainPage.js
+++ src/Components/Home/Dashboard/Home/Graphs/GraphsMainPage.js
@@ -1,249 +1,281 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Donuts from './GraphsLists/Donuts';
-import Bars from './GraphsLists/Bars';
+import Donuts from "./GraphsLists/Donuts";
+import Bars from "./GraphsLists/Bars";
 
-import styled from 'styled-components';
-import { Request_Get_Axios } from '../../../../../API';
-import { Getting_Top6_Recent_Sell_Equipments_Lists } from '../../../../../Models/ReduxThunks/EISDashbaord/Graphs/RecentEquipmentsThunkReducers';
-import { useDispatch, useSelector } from 'react-redux';
-import ClipLoaders from '../../../../../Loader/ClipLoader';
-import moment from 'moment';
+import styled from "styled-components";
+import { Request_Get_Axios } from "../../../../../API";
+import { Getting_Top6_Recent_Sell_Equipments_Lists } from "../../../../../Models/ReduxThunks/EISDashbaord/Graphs/RecentEquipmentsThunkReducers";
+import { useDispatch, useSelector } from "react-redux";
+import ClipLoaders from "../../../../../Loader/ClipLoader";
+import moment from "moment";
 
 const GraphsMainPageMainDivBox = styled.div`
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-around;
-    .Graph_Container_GR {
-        border: 1px solid lightgray;
-        height: calc(100vh - 350px);
-        background-color: #fff;
-        box-shadow: -8px 8px 3px -5px lightgray;
-        border-top: 4px solid rgb(0, 202, 255);
-        border-bottom-left-radius: 5px;
-        border-bottom-right-radius: 5px;
-        position: relative;
-        .Select_Group {
-            position: absolute;
-            top: 10px;
-            display: flex;
-            justify-content: space-between;
-            width: 90%;
-            z-index: 10000;
-            left: 40px;
-            h3 {
-                font-size: 1.5em;
-                font-weight: bolder;
-            }
-            .Unit_Container {
-                position: absolute;
-                top: 40px;
-                right: 0px;
-                font-weight: bolder;
-            }
-        }
-    }
-
-    .Select_Buttons {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  .Graph_Container_GR {
+    border: 1px solid lightgray;
+    height: calc(100vh - 350px);
+    background-color: #fff;
+    box-shadow: -8px 8px 3px -5px lightgray;
+    border-top: 4px solid rgb(0, 202, 255);
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    position: relative;
+    .Select_Group {
+      position: absolute;
+      top: 10px;
+      display: flex;
+      justify-content: space-between;
+      width: 90%;
+      z-index: 10000;
+      left: 40px;
+      h3 {
+        font-size: 1.5em;
+        font-weight: bolder;
+      }
+      .Unit_Container {
         position: absolute;
         top: 40px;
-        left: 0px;
-        .NowSelected {
-            opacity: 1;
-            border: 2px solid blue;
-        }
-        button {
-            width: 60px;
-            height: 40px;
-            border: 1px solid lightgray;
-            border-radius: 10px;
-            margin-right: 10px;
-            background-color: #fff;
-            font-size: 16px;
-            font-weight: bolder;
-            opacity: 0.5;
-            &:hover {
-                cursor: pointer;
-            }
-        }
+        right: 0px;
+        font-weight: bolder;
+      }
     }
+  }
+
+  .Select_Buttons {
+    position: absolute;
+    top: 40px;
+    left: 0px;
+    .NowSelected {
+      opacity: 1;
+      border: 2px solid blue;
+    }
+    button {
+      width: 60px;
+      height: 40px;
+      border: 1px solid lightgray;
+      border-radius: 10px;
+      margin-right: 10px;
+      background-color: #fff;
+      font-size: 16px;
+      font-weight: bolder;
+      opacity: 0.5;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
 `;
 
 const GraphsMainPage = () => {
-    const Pie_State = useSelector(state => state.Pie_Equipments_Sell_Thunk_Reducers_State);
-    const DepartMentLists_State = useSelector(state => state.McAverage_ThunkReducers_State);
-    const Select_Date_State = useSelector(state => state.Select_Date_Reducer_State.Select_Date_State);
-    const [ClickData, setClickData] = useState(null);
-    const [Bar_State, setBar_State] = useState([]);
-    const [Pie_State_By_Selector, setPie_State_By_Selector] = useState([]);
-    const [NowSelectGraphButton, setNowSelectGraphButton] = useState('Equipment');
+  const Pie_State = useSelector(
+    (state) => state.Pie_Equipments_Sell_Thunk_Reducers_State
+  );
+  const DepartMentLists_State = useSelector(
+    (state) => state.McAverage_ThunkReducers_State
+  );
+  const Select_Date_State = useSelector(
+    (state) => state.Select_Date_Reducer_State.Select_Date_State
+  );
+  const [ClickData, setClickData] = useState(null);
+  const [Bar_State, setBar_State] = useState([]);
+  const [Pie_State_By_Selector, setPie_State_By_Selector] = useState([]);
+  const [NowSelectGraphButton, setNowSelectGraphButton] = useState("Equipment");
 
-    const [Select_Value, setSelect_Value] = useState('ALL');
-    const [Pie_Selector_List, setPie_Selector_List] = useState([
-        {
-            value: 'ALL',
-            label: '전체',
-        },
-        {
-            value: 'Module',
-            label: 'DC/Module',
-        },
-        {
-            value: 'MBT',
-            label: 'MBT',
-        },
-        {
-            value: 'Storage',
-            label: 'Storage',
-        },
-        {
-            value: 'SoC',
-            label: 'SOC',
-        },
-        {
-            value: 'CLT',
-            label: 'CLT',
-        },
-    ]);
-    const HandleChangeOptions = e => {
-        e.preventDefault();
+  const [Select_Value, setSelect_Value] = useState("ALL");
+  const [Pie_Selector_List, setPie_Selector_List] = useState([
+    {
+      value: "ALL",
+      label: "전체",
+    },
+    {
+      value: "Module",
+      label: "DC/Module",
+    },
+    {
+      value: "MBT",
+      label: "MBT",
+    },
+    {
+      value: "Storage",
+      label: "Storage",
+    },
+    {
+      value: "SoC",
+      label: "SOC",
+    },
+    {
+      value: "CLT",
+      label: "CLT",
+    },
+  ]);
+  const HandleChangeOptions = (e) => {
+    e.preventDefault();
 
-        setSelect_Value(e.target.value);
-    };
+    setSelect_Value(e.target.value);
+  };
 
-    useEffect(() => {
-        if (Select_Value === 'ALL') {
-            setPie_State_By_Selector(Pie_State.PieData);
-        } else {
-            Grouping_Data();
-        }
-    }, [Select_Value, Select_Date_State.value, Pie_State.PieData]);
+  useEffect(() => {
+    if (Select_Value === "ALL") {
+      setPie_State_By_Selector(Pie_State.PieData);
+    } else {
+      Grouping_Data();
+    }
+  }, [Select_Value, Select_Date_State.value, Pie_State.PieData]);
 
-    useEffect(() => {
-        setClickData(null);
-    }, [Select_Date_State.value]);
+  useEffect(() => {
+    setClickData(null);
+  }, [Select_Date_State.value]);
 
-    useEffect(() => {
-        if (DepartMentLists_State) {
-            if (DepartMentLists_State?.DepartMentLists.length > 0) {
-                setBar_State(
-                    DepartMentLists_State?.DepartMentLists.filter(item => item.Segment === ClickData || item.Models === ClickData).map(
-                        list => {
-                            return {
-                                id: list.WO_NO,
-                                equipments: `${list.Models}`,
-                                MC: Number(
-                                    (list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
-                                        ? list.MC_Price / 1000000
-                                        : (list.MC_Price / 1000000) * list.QTY
-                                ),
-                                price: list.EXPC_SEL_PRICE / 1000000,
-                                dueDate: list.DUE_DT,
-                                WO_TYPE: list.WO_TYPE,
-                                QTY: list.QTY,
-                                CHNG_CONT: list.CHNG_CONT,
-                            };
-                        }
-                    )
-                );
-            } else {
-                setBar_State([]);
-            }
-        } else {
-            setBar_State([]);
-        }
-    }, [DepartMentLists_State.DepartMentLists, ClickData]);
-
-    const Grouping_Data = () => {
-        const Grouping_Models = [];
-        const datas = Pie_State.PieData.map(list => {
-            if (list.code === Select_Value) {
-                return list.Lists.map(item => {
-                    return Grouping_Models.push(item.Models);
-                });
-            }
-        });
-        const Delete_Duple_Data = [...new Set(Grouping_Models)];
-
-        const Pies_data = Delete_Duple_Data.map(list => {
-            const [abc] = Pie_State.PieData.filter(items => items.code === Select_Value);
+  useEffect(() => {
+    if (DepartMentLists_State) {
+      if (DepartMentLists_State?.DepartMentLists.length > 0) {
+        setBar_State(
+          DepartMentLists_State?.DepartMentLists.filter(
+            (item) => item.Segment === ClickData || item.Models === ClickData
+          ).map((list) => {
             return {
-                id: list,
-                label: list,
-                code: list,
-                value: abc.Lists.filter(items => items.Models === list).reduce((pre, acc) => pre + acc.EXPC_SEL_PRICE / 1000000, 0),
+              id: list.WO_NO,
+              equipments: `${list.Models}`,
+              MC: Number(
+                (list.MC_Price / list.EXPC_SEL_PRICE) * list.QTY * 100 > 100
+                  ? (list.MC_Price + list.outSoucingPriceSum) / 1000000
+                  : (list.MC_Price * list.QTY + list.outSoucingPriceSum) /
+                      1000000
+              ),
+              price: list.EXPC_SEL_PRICE / 1000000,
+              dueDate: list.DUE_DT,
+              WO_TYPE: list.WO_TYPE,
+              QTY: list.QTY,
+              CHNG_CONT: list.CHNG_CONT,
             };
+          })
+        );
+      } else {
+        setBar_State([]);
+      }
+    } else {
+      setBar_State([]);
+    }
+  }, [DepartMentLists_State.DepartMentLists, ClickData]);
+
+  const Grouping_Data = () => {
+    const Grouping_Models = [];
+    const datas = Pie_State.PieData.map((list) => {
+      if (list.code === Select_Value) {
+        return list.Lists.map((item) => {
+          return Grouping_Models.push(item.Models);
         });
-        setPie_State_By_Selector(Pies_data);
-    };
+      }
+    });
+    const Delete_Duple_Data = [...new Set(Grouping_Models)];
 
-    return (
-        <GraphsMainPageMainDivBox>
-            <div className="Graph_Container_GR" style={{ width: '38%' }}>
-                {/* <h3>매출액({Pie_State_By_Selector.reduce((pre, acc) => pre + acc.value, 0).toLocaleString('ko-kr')} M)</h3> */}
-                <div className="Select_Group">
-                    <h3>
-                        {moment(Select_Date_State.value).format('YY')}년 수주액{' '}
-                        <span style={{ color: 'blue', fontSize: '0.8em' }}>
-                            {' '}
-                            : {Pie_State_By_Selector.reduce((pre, acc) => pre + acc.value, 0).toLocaleString('ko-kr')} M
-                        </span>
-                    </h3>
-                    <select className="Select_Container" onChange={e => HandleChangeOptions(e)} value={Select_Value}>
-                        {Pie_Selector_List.map(list => {
-                            return (
-                                <option key={list.value} value={list.value}>
-                                    {list.label}
-                                </option>
-                            );
-                        })}
-                    </select>
-                    <div className="Unit_Container">단위 : 원(&#8361;)</div>
-                </div>
+    const Pies_data = Delete_Duple_Data.map((list) => {
+      const [abc] = Pie_State.PieData.filter(
+        (items) => items.code === Select_Value
+      );
+      return {
+        id: list,
+        label: list,
+        code: list,
+        value: abc.Lists.filter((items) => items.Models === list).reduce(
+          (pre, acc) => pre + acc.EXPC_SEL_PRICE / 1000000,
+          0
+        ),
+      };
+    });
+    setPie_State_By_Selector(Pies_data);
+  };
 
-                {Pie_State.loading ? (
-                    <ClipLoaders loading={Pie_State.loading}></ClipLoaders>
-                ) : (
-                    <Donuts
-                        Pie_State={Pie_State_By_Selector.filter(item => item.value > 0)}
-                        setClickData={data => setClickData(data)}
-                    ></Donuts>
-                )}
+  return (
+    <GraphsMainPageMainDivBox>
+      <div className="Graph_Container_GR" style={{ width: "38%" }}>
+        {/* <h3>매출액({Pie_State_By_Selector.reduce((pre, acc) => pre + acc.value, 0).toLocaleString('ko-kr')} M)</h3> */}
+        <div className="Select_Group">
+          <h3>
+            {moment(Select_Date_State.value).format("YY")}년 수주액{" "}
+            <span style={{ color: "blue", fontSize: "0.8em" }}>
+              {" "}
+              :{" "}
+              {Pie_State_By_Selector.reduce(
+                (pre, acc) => pre + acc.value,
+                0
+              ).toLocaleString("ko-kr")}{" "}
+              M
+            </span>
+          </h3>
+          <select
+            className="Select_Container"
+            onChange={(e) => HandleChangeOptions(e)}
+            value={Select_Value}
+          >
+            {Pie_Selector_List.map((list) => {
+              return (
+                <option key={list.value} value={list.value}>
+                  {list.label}
+                </option>
+              );
+            })}
+          </select>
+          <div className="Unit_Container">단위 : 원(&#8361;)</div>
+        </div>
 
-                {/* {Pie_State_By_Selector.length > 0 ? (
+        {Pie_State.loading ? (
+          <ClipLoaders loading={Pie_State.loading}></ClipLoaders>
+        ) : (
+          <Donuts
+            Pie_State={Pie_State_By_Selector.filter((item) => item.value > 0)}
+            setClickData={(data) => setClickData(data)}
+          ></Donuts>
+        )}
+
+        {/* {Pie_State_By_Selector.length > 0 ? (
                     <Donuts Pie_State={Pie_State_By_Selector.filter(item => item.value > 0)}></Donuts>
                 ) : (
                     <></>
                 )} */}
-            </div>
-            <div className="Graph_Container_GR" style={{ width: '58%' }}>
-                <div className="Select_Group">
-                    <h3>{ClickData} 최근 판매 제품 판가 및 MC</h3>
-                    <div className="Select_Buttons">
-                        <button
-                            className={NowSelectGraphButton === 'Equipment' && ClickData ? 'NowSelected' : ''}
-                            onClick={() => setNowSelectGraphButton('Equipment')}
-                        >
-                            {' '}
-                            장 비{' '}
-                        </button>
-                        <button
-                            className={NowSelectGraphButton === 'Board' && ClickData ? 'NowSelected' : ''}
-                            onClick={() => setNowSelectGraphButton('Board')}
-                        >
-                            {' '}
-                            보 드{' '}
-                        </button>
-                    </div>
-                </div>
-                {Bar_State.loading ? (
-                    <ClipLoaders loading={Bar_State.loading}></ClipLoaders>
-                ) : (
-                    <Bars Bar_State={Bar_State} NowSelectGraphButton={NowSelectGraphButton}></Bars>
-                )}
-            </div>
-        </GraphsMainPageMainDivBox>
-    );
+      </div>
+      <div className="Graph_Container_GR" style={{ width: "58%" }}>
+        <div className="Select_Group">
+          <h3>{ClickData} 최근 판매 제품 판가 및 MC</h3>
+          <div className="Select_Buttons">
+            <button
+              className={
+                NowSelectGraphButton === "Equipment" && ClickData
+                  ? "NowSelected"
+                  : ""
+              }
+              onClick={() => setNowSelectGraphButton("Equipment")}
+            >
+              {" "}
+              장 비{" "}
+            </button>
+            <button
+              className={
+                NowSelectGraphButton === "Board" && ClickData
+                  ? "NowSelected"
+                  : ""
+              }
+              onClick={() => setNowSelectGraphButton("Board")}
+            >
+              {" "}
+              보 드{" "}
+            </button>
+          </div>
+        </div>
+        {Bar_State.loading ? (
+          <ClipLoaders loading={Bar_State.loading}></ClipLoaders>
+        ) : (
+          <Bars
+            Bar_State={Bar_State}
+            NowSelectGraphButton={NowSelectGraphButton}
+          ></Bars>
+        )}
+      </div>
+    </GraphsMainPageMainDivBox>
+  );
 };
 
 export default GraphsMainPage;
